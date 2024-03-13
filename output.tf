02e997111f2af410f4330c5fd8c9718f91bac6f9
serge <kserge2001@yahoo.fr>
2024-03-12 21:25:04 -0500

    route53

--- output.tf
+++ output.tf
@@ -9,3 +9,7 @@ output "ssh-command" {
   value = "ssh -i ${local_file.ssh_key.filename} ${aws_lightsail_instance.server1.username}@${aws_lightsail_instance.server1.public_ip_address}"
 }
 
+
+output "dns-name" {
+  value = aws_route53_record.rc1.name
+}
